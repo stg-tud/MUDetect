3a85606f654c94bdd1c5ed33b1f274ee0373f562
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-08-22 09:24:43 +0200

    Fix NPE.

--- src/exas/ExasFeature.java
+++ src/exas/ExasFeature.java
@@ -19,6 +19,7 @@ public class ExasFeature {
 		edgeFeatures.put(EGroumDataEdge.getLabel(Type.RECEIVER), edgeFeatures.size());
 		edgeFeatures.put(EGroumDataEdge.getLabel(Type.REFERENCE), edgeFeatures.size());
 		edgeFeatures.put(EGroumDataEdge.getLabel(Type.THROW), edgeFeatures.size());
+		edgeFeatures.put(EGroumDataEdge.getLabel(Type.FINALLY), edgeFeatures.size());
 		edgeFeatures.put("_control_", edgeFeatures.size());
 	}
 	
