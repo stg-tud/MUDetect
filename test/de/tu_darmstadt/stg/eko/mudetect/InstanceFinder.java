d164c91b05f6523259db74e290508910f0c06bd7
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-09-13 13:23:15 +0200

    Introduce a work queue to reduce duplication.

--- test/de/tu_darmstadt/stg/eko/mudetect/InstanceFinder.java
+++ test/de/tu_darmstadt/stg/eko/mudetect/InstanceFinder.java
@@ -3,54 +3,77 @@ package de.tu_darmstadt.stg.eko.mudetect;
 import de.tu_darmstadt.stg.eko.mudetect.model.AUG;
 import egroum.EGroumActionNode;
 import egroum.EGroumDataNode;
+import egroum.EGroumGraph;
 import egroum.EGroumNode;
 
 import java.util.*;
 
 public class InstanceFinder {
-    private static class CommonNode<T extends EGroumNode> {
-        private T targetNode;
-        private T patternNode;
+    private static class WorkItem {
+        AUG pattern;
+        EGroumNode patternNode;
+        AUG target;
+        EGroumNode targetNode;
 
-        CommonNode(T targetNode, T patternNode) {
-            this.targetNode = targetNode;
+        WorkItem(AUG pattern, EGroumNode patternNode, AUG target, EGroumNode targetNode) {
+            this.pattern = pattern;
             this.patternNode = patternNode;
+            this.target = target;
+            this.targetNode = targetNode;
+        }
+    }
+
+    private static class WorkQueue {
+        private Map<EGroumNode, WorkItem> workItemsByPatternNode = new HashMap<>();
+
+        void add(WorkItem item) {
+            if (workItemsByPatternNode.containsKey(item.patternNode)) {
+                throw new IllegalStateException("adding a second item for the same pattern node");
+            }
+            workItemsByPatternNode.put(item.patternNode, item);
+        }
+
+        boolean hasNext() {
+            return !workItemsByPatternNode.isEmpty();
+        }
+
+        WorkItem poll() {
+            Iterator<Map.Entry<EGroumNode, WorkItem>> iterator = workItemsByPatternNode.entrySet().iterator();
+            Map.Entry<EGroumNode, WorkItem> next = iterator.next();
+            workItemsByPatternNode.remove(next.getKey());
+            return next.getValue();
+        }
+
+        void remove(EGroumNode patternNode) {
+            workItemsByPatternNode.remove(patternNode);
         }
     }
 
     public static List<Instance> findInstances(AUG target, AUG pattern) {
         Map<String, Set<EGroumActionNode>> patternNodesByLabel = getMeaningfulActionNodesByLabel(pattern);
 
-        Set<CommonNode<EGroumActionNode>> initialNodes = new HashSet<>();
+        WorkQueue queue = new WorkQueue();
         for (EGroumNode targetNode : target.vertexSet()) {
             String label = targetNode.getLabel();
             if (patternNodesByLabel.containsKey(label)) {
                 for (EGroumActionNode patternNode : patternNodesByLabel.get(label)) {
-                    initialNodes.add(new CommonNode<>((EGroumActionNode) targetNode, patternNode));
+                    queue.add(new WorkItem(pattern, patternNode, target, targetNode));
                 }
-
             }
         }
 
         List<Instance> instances = new ArrayList<>();
-        while (!initialNodes.isEmpty()) {
-            CommonNode<EGroumActionNode> node = poll(initialNodes);
+        while (queue.hasNext()) {
+            WorkItem item = queue.poll();
             Instance instance = new Instance(pattern, new HashSet<>());
-            extendInstance(instance, target, node.targetNode, pattern, node.patternNode, initialNodes);
+            extendInstance(instance, item.target, (EGroumActionNode) item.targetNode, item.pattern, (EGroumActionNode) item.patternNode, queue);
             instances.add(instance);
         }
 
         return instances;
     }
 
-    private static CommonNode<EGroumActionNode> poll(Set<CommonNode<EGroumActionNode>> initialNodes) {
-        Iterator<CommonNode<EGroumActionNode>> iterator = initialNodes.iterator();
-        CommonNode<EGroumActionNode> node = iterator.next();
-        iterator.remove();
-        return node;
-    }
-
-    private static void extendInstance(Instance instance, AUG target, EGroumActionNode targetNode, AUG pattern, EGroumActionNode patternNode, Set<CommonNode<EGroumActionNode>> initialNodes) {
+    private static void extendInstance(Instance instance, AUG target, EGroumActionNode targetNode, AUG pattern, EGroumActionNode patternNode, WorkQueue queue) {
         instance.addVertex(patternNode);
 
         EGroumDataNode patternReceiver = pattern.getReceiver(patternNode);
@@ -59,7 +82,7 @@ public class InstanceFinder {
             if (targetReceiver.getLabel().equals(patternReceiver.getLabel())) {
                 instance.addVertex(patternReceiver);
                 instance.addEdge(patternReceiver, patternNode, pattern.getEdge(patternReceiver, patternNode));
-                extendInstance(instance, target, targetReceiver, pattern, patternReceiver, initialNodes);
+                extendInstance(instance, target, targetReceiver, pattern, patternReceiver, queue);
             }
         }
 
@@ -70,19 +93,14 @@ public class InstanceFinder {
                 if (patternCondition.getLabel().equals(targetCondition.getLabel())) {
                     instance.addVertex(patternCondition);
                     instance.addEdge(patternCondition, patternNode, pattern.getEdge(patternCondition, patternNode));
-                    extendInstance(instance, target, targetCondition, pattern, patternCondition, initialNodes);
-                    for (Iterator<CommonNode<EGroumActionNode>> it = initialNodes.iterator(); it.hasNext();) {
-                        CommonNode<EGroumActionNode> node = it.next();
-                        if (node.patternNode == patternCondition) {
-                            it.remove();
-                        }
-                    }
+                    extendInstance(instance, target, targetCondition, pattern, patternCondition, queue);
+                    queue.remove(patternCondition);
                 }
             }
         }
     }
 
-    private static void extendInstance(Instance instance, AUG target, EGroumDataNode targetNode, AUG pattern, EGroumDataNode patternNode, Set<CommonNode<EGroumActionNode>> initialNodes) {
+    private static void extendInstance(Instance instance, AUG target, EGroumDataNode targetNode, AUG pattern, EGroumDataNode patternNode, WorkQueue queue) {
         Set<EGroumActionNode> patternInvocations = pattern.getInvocations(patternNode);
         Set<EGroumActionNode> targetInvocations = target.getInvocations(targetNode);
         for (EGroumActionNode patternInvocation : patternInvocations) {
@@ -91,14 +109,8 @@ public class InstanceFinder {
                     if (targetInvocation.getLabel().equals(patternInvocation.getLabel())) {
                         instance.addVertex(patternInvocation);
                         instance.addEdge(patternNode, patternInvocation, pattern.getEdge(patternNode, patternInvocation));
-                        extendInstance(instance, target, targetInvocation, pattern, patternInvocation, initialNodes);
-
-                        for (Iterator<CommonNode<EGroumActionNode>> it = initialNodes.iterator(); it.hasNext();) {
-                            CommonNode<EGroumActionNode> node = it.next();
-                            if (node.patternNode == patternInvocation) {
-                                it.remove();
-                            }
-                        }
+                        extendInstance(instance, target, targetInvocation, pattern, patternInvocation, queue);
+                        queue.remove(patternInvocation);
                     }
                 }
 
