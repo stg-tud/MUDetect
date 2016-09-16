4aab660a99e7dc89d94451cf744319f459a7cc78
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-09-16 17:28:11 +0200

    Remove pattern field.

--- test/de/tu_darmstadt/stg/mudetect/Instance.java
+++ test/de/tu_darmstadt/stg/mudetect/Instance.java
@@ -12,7 +12,6 @@ import java.util.*;
 
 public class Instance extends Subgraph<EGroumNode, EGroumEdge, AUG> {
 
-    private final AUG pattern;
     private final AUG target;
 
     /**
@@ -20,49 +19,51 @@ public class Instance extends Subgraph<EGroumNode, EGroumEdge, AUG> {
      */
     public Instance(AUG pattern, Set<EGroumNode> vertexSubset, Set<EGroumEdge> edgeSubset) {
         super(pattern, vertexSubset, edgeSubset);
-        this.pattern = pattern;
         this.target = pattern;
     }
 
     Instance(AUG pattern, AUG target) {
         super(pattern, new HashSet<>());
-        this.pattern = pattern;
         this.target = target;
+    }
+
+    public AUG getPattern() {
+        return getBase();
     }
 
     void extend(EGroumActionNode targetNode, EGroumActionNode patternNode) {
         addVertex(patternNode);
 
-        EGroumDataNode patternReceiver = pattern.getReceiver(patternNode);
+        EGroumDataNode patternReceiver = getPattern().getReceiver(patternNode);
         if (patternReceiver != null) {
             EGroumDataNode targetReceiver = target.getReceiver(targetNode);
             if (targetReceiver.getLabel().equals(patternReceiver.getLabel())) {
                 addVertex(patternReceiver);
-                addEdge(patternReceiver, patternNode, pattern.getEdge(patternReceiver, patternNode));
+                addEdge(patternReceiver, patternNode, getPattern().getEdge(patternReceiver, patternNode));
                 extend(patternReceiver, targetReceiver);
             }
         }
 
-        Set<Condition> patternConditions = pattern.getConditions(patternNode);
+        Set<Condition> patternConditions = getPattern().getConditions(patternNode);
         Set<Condition> targetConditions = target.getConditions(targetNode);
         for (Condition patternCondition : patternConditions) {
             for (Condition targetCondition : targetConditions) {
                 if (targetCondition.isInstanceOf(patternCondition)) {
                     EGroumActionNode patternConditionNode = patternCondition.getNode();
                     addVertex(patternConditionNode);
-                    addEdge(patternConditionNode, patternNode, pattern.getEdge(patternConditionNode, patternNode));
+                    addEdge(patternConditionNode, patternNode, getPattern().getEdge(patternConditionNode, patternNode));
                     extend(targetCondition.getNode(), patternConditionNode);
                 }
             }
         }
 
-        Set<EGroumNode> patternArguments = pattern.getArguments(patternNode);
+        Set<EGroumNode> patternArguments = getPattern().getArguments(patternNode);
         Set<EGroumNode> targetArguments = target.getArguments(targetNode);
         for (EGroumNode patternArgument : patternArguments) {
             for (EGroumNode targetArgument : targetArguments) {
                 if (targetArgument.getLabel().equals(patternArgument.getLabel())) {
                     addVertex(patternArgument);
-                    addEdge(patternArgument, patternNode, pattern.getEdge(patternArgument, patternNode));
+                    addEdge(patternArgument, patternNode, getPattern().getEdge(patternArgument, patternNode));
                     if (patternArgument instanceof EGroumDataNode) {
                         extend((EGroumDataNode) targetArgument, (EGroumDataNode) patternArgument);
                     } else {
@@ -74,14 +75,14 @@ public class Instance extends Subgraph<EGroumNode, EGroumEdge, AUG> {
     }
 
     private void extend(EGroumDataNode targetNode, EGroumDataNode patternNode) {
-        Set<EGroumActionNode> patternInvocations = pattern.getInvocations(patternNode);
+        Set<EGroumActionNode> patternInvocations = getPattern().getInvocations(patternNode);
         Set<EGroumActionNode> targetInvocations = target.getInvocations(targetNode);
         for (EGroumActionNode patternInvocation : patternInvocations) {
             if (!containsVertex(patternInvocation)) {
                 for (EGroumActionNode targetInvocation : targetInvocations) {
                     if (targetInvocation.getLabel().equals(patternInvocation.getLabel())) {
                         addVertex(patternInvocation);
-                        addEdge(patternNode, patternInvocation, pattern.getEdge(patternNode, patternInvocation));
+                        addEdge(patternNode, patternInvocation, getPattern().getEdge(patternNode, patternInvocation));
                         extend(targetInvocation, patternInvocation);
                     }
                 }
