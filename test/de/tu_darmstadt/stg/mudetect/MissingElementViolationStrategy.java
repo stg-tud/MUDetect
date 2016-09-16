beb9d53f59b7654745ed1128647ef87cf4107e90
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-09-16 17:28:46 +0200

    Implement naive violation strategy.

---
+++ test/de/tu_darmstadt/stg/mudetect/MissingElementViolationStrategy.java
@@ -0,0 +1,12 @@
+package de.tu_darmstadt.stg.mudetect;
+
+import de.tu_darmstadt.stg.mudetect.model.AUG;
+
+class MissingElementViolationStrategy implements ViolationStrategy {
+    @Override
+    public boolean isViolation(Instance instance) {
+        AUG pattern = instance.getPattern();
+        return instance.vertexSet().size() < pattern.vertexSet().size() ||
+                instance.edgeSet().size() < pattern.edgeSet().size();
+    }
+}
