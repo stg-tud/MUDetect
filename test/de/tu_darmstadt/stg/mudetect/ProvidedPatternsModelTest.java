e423ea1d14160ae8d71f28206898abc815810ecc
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-09-16 18:23:16 +0200

    Implement provided-patterns model.

---
+++ test/de/tu_darmstadt/stg/mudetect/ProvidedPatternsModelTest.java
@@ -0,0 +1,24 @@
+package de.tu_darmstadt.stg.mudetect;
+
+import egroum.EGroumGraph;
+import org.junit.Test;
+
+import java.util.List;
+
+import static egroum.EGroumTestUtils.buildGroumsForClass;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+public class ProvidedPatternsModelTest {
+    @Test
+    public void convertsGroumToPatterns() throws Exception {
+        List<EGroumGraph> groums = buildGroumsForClass("class A {" +
+                "  void m(C c) { c.foo(); }" +
+                "  void n(C c) { c.bar(); }" +
+                "}");
+
+        Model model = new ProvidedPatternsModel(groums);
+
+        assertThat(model.getPatterns(), hasSize(2));
+    }
+}
--- test/de/tu_darmstadt/stg/mudetect/model/Model.java
+++
@@ -1,7 +0,0 @@
-package de.tu_darmstadt.stg.mudetect.model;
-
-import java.util.Set;
-
-public interface Model {
-    Set<Pattern> getPatterns();
-}
