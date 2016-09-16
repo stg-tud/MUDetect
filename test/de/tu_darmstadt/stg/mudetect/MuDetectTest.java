381d5794fd0bb60fd4d0bf0a96557cf8c70ac746
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-09-16 17:14:35 +0200

    Test filters non-violations.

--- test/de/tu_darmstadt/stg/mudetect/MuDetectTest.java
+++ test/de/tu_darmstadt/stg/mudetect/MuDetectTest.java
@@ -12,6 +12,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -45,6 +46,32 @@ public class MuDetectTest {
         assertThat(violations, hasSize(1));
         Violation violation = violations.get(0);
         assertThat(violation.getInstance(), is(instance));
+    }
+
+    @Test
+    public void ignoresNonViolations() throws Exception {
+        final Model model = context.mock(Model.class);
+        final Pattern pattern = new Pattern(new AUG());
+        final AUG target = new AUG();
+        final Collection<AUG> targets = Collections.singletonList(target);
+        final Instance instance = new Instance(pattern.getAUG(), target);
+        final InstanceFinder instanceFinder = context.mock(InstanceFinder.class);
+        final ViolationStrategy violationStrategy = context.mock(ViolationStrategy.class);
+        context.checking(new Expectations() {{
+            allowing(model).getPatterns();
+            will(returnValue(Collections.singleton(pattern)));
+
+            allowing(instanceFinder).findInstances(pattern.getAUG(), target);
+            will(returnValue(Collections.singletonList(instance)));
+
+            allowing(violationStrategy).isViolation(instance);
+            will(returnValue(false));
+        }});
+
+        MuDetect muDetect = new MuDetect(model, instanceFinder, violationStrategy);
+        List<Violation> violations = muDetect.findViolations(targets);
+
+        assertThat(violations, is(empty()));
     }
 
 }
