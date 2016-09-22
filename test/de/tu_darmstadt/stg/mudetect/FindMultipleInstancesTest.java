4f22c6661773a6340d2811ba1ae1189ff557bc92
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-09-22 14:20:30 +0200

    Move inner class to upper level.

--- test/de/tu_darmstadt/stg/mudetect/FindMultipleInstancesTest.java
+++ test/de/tu_darmstadt/stg/mudetect/FindMultipleInstancesTest.java
@@ -2,17 +2,13 @@ package de.tu_darmstadt.stg.mudetect;
 
 import de.tu_darmstadt.stg.mudetect.model.AUG;
 import de.tu_darmstadt.stg.mudetect.model.AUGBuilder;
-import egroum.EGroumDataEdge;
-import egroum.EGroumEdge;
-import egroum.EGroumNode;
 import org.junit.Test;
 
 import java.util.*;
 
-import static de.tu_darmstadt.stg.mudetect.FindMultipleInstancesTest.InstanceBuilder.createInstance;
+import static de.tu_darmstadt.stg.mudetect.InstanceBuilder.createInstance;
 import static de.tu_darmstadt.stg.mudetect.model.AUGBuilder.buildAUG;
 import static de.tu_darmstadt.stg.mudetect.model.AUGBuilder.extend;
-import static de.tu_darmstadt.stg.mudetect.model.InstanceTestUtils.hasInstance;
 import static egroum.EGroumDataEdge.Type.ORDER;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
@@ -43,44 +39,4 @@ public class FindMultipleInstancesTest {
         assertThat(instances, hasItems(instance1, instance2));
     }
 
-    static class InstanceBuilder {
-        private AUGBuilder targetAUGBuilder;
-        private AUGBuilder patternAUGBuilder;
-
-        public static InstanceBuilder createInstance(AUGBuilder targetAUGBuilder, AUGBuilder patternAUGBuilder) {
-            return new InstanceBuilder(targetAUGBuilder, patternAUGBuilder);
-        }
-
-        private final Map<EGroumNode, EGroumNode> targetNodeByPatternNode = new HashMap<>();
-        private final Map<EGroumEdge, EGroumEdge> targetEdgeByPatternEdge = new HashMap<>();
-
-        private InstanceBuilder(AUGBuilder targetAUGBuilder, AUGBuilder patternAUGBuilder) {
-            this.targetAUGBuilder = targetAUGBuilder;
-            this.patternAUGBuilder = patternAUGBuilder;
-        }
-
-        public InstanceBuilder withNode(String targetNodeId, String patternNodeId) {
-            EGroumNode targetNode = targetAUGBuilder.getNode(targetNodeId);
-            if (targetNodeByPatternNode.containsValue(targetNode)) {
-                throw new IllegalArgumentException("Target node '" + targetNodeId + "' is already mapped.");
-            }
-            EGroumNode patternNode = patternAUGBuilder.getNode(patternNodeId);
-            if (targetNodeByPatternNode.containsKey(patternNode)) {
-                throw new IllegalArgumentException("Pattern node '" + patternNodeId + "' is already mapped.");
-            }
-            targetNodeByPatternNode.put(patternNode, targetNode);
-            return this;
-        }
-
-        public InstanceBuilder withEdge(String targetSourceNodeId, String patternSourceNodeId, EGroumDataEdge.Type type, String targetTargetNodeId, String patternTargetNodeId) {
-            targetEdgeByPatternEdge.put(
-                    patternAUGBuilder.getEdge(patternSourceNodeId, type, patternTargetNodeId),
-                    targetAUGBuilder.getEdge(targetSourceNodeId, type, targetTargetNodeId));
-            return this;
-        }
-
-        public Instance build() {
-            return new Instance(targetAUGBuilder.build(), patternAUGBuilder.build(), targetNodeByPatternNode, targetEdgeByPatternEdge);
-        }
-    }
 }
---
+++ test/de/tu_darmstadt/stg/mudetect/InstanceBuilder.java
@@ -0,0 +1,50 @@
+package de.tu_darmstadt.stg.mudetect;
+
+import de.tu_darmstadt.stg.mudetect.model.AUGBuilder;
+import egroum.EGroumDataEdge;
+import egroum.EGroumEdge;
+import egroum.EGroumNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class InstanceBuilder {
+    private AUGBuilder targetAUGBuilder;
+    private AUGBuilder patternAUGBuilder;
+
+    public static InstanceBuilder createInstance(AUGBuilder targetAUGBuilder, AUGBuilder patternAUGBuilder) {
+        return new InstanceBuilder(targetAUGBuilder, patternAUGBuilder);
+    }
+
+    private final Map<EGroumNode, EGroumNode> targetNodeByPatternNode = new HashMap<>();
+    private final Map<EGroumEdge, EGroumEdge> targetEdgeByPatternEdge = new HashMap<>();
+
+    private InstanceBuilder(AUGBuilder targetAUGBuilder, AUGBuilder patternAUGBuilder) {
+        this.targetAUGBuilder = targetAUGBuilder;
+        this.patternAUGBuilder = patternAUGBuilder;
+    }
+
+    public InstanceBuilder withNode(String targetNodeId, String patternNodeId) {
+        EGroumNode targetNode = targetAUGBuilder.getNode(targetNodeId);
+        if (targetNodeByPatternNode.containsValue(targetNode)) {
+            throw new IllegalArgumentException("Target node '" + targetNodeId + "' is already mapped.");
+        }
+        EGroumNode patternNode = patternAUGBuilder.getNode(patternNodeId);
+        if (targetNodeByPatternNode.containsKey(patternNode)) {
+            throw new IllegalArgumentException("Pattern node '" + patternNodeId + "' is already mapped.");
+        }
+        targetNodeByPatternNode.put(patternNode, targetNode);
+        return this;
+    }
+
+    public InstanceBuilder withEdge(String targetSourceNodeId, String patternSourceNodeId, EGroumDataEdge.Type type, String targetTargetNodeId, String patternTargetNodeId) {
+        targetEdgeByPatternEdge.put(
+                patternAUGBuilder.getEdge(patternSourceNodeId, type, patternTargetNodeId),
+                targetAUGBuilder.getEdge(targetSourceNodeId, type, targetTargetNodeId));
+        return this;
+    }
+
+    public Instance build() {
+        return new Instance(targetAUGBuilder.build(), patternAUGBuilder.build(), targetNodeByPatternNode, targetEdgeByPatternEdge);
+    }
+}
