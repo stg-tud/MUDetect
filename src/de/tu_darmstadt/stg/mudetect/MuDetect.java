4ff72e8f182371dbc8cf82ec46b01215a8dd204f
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-10-11 17:37:22 +0200

    Filter instances of alternative patterns.

--- src/de/tu_darmstadt/stg/mudetect/MuDetect.java
+++ src/de/tu_darmstadt/stg/mudetect/MuDetect.java
@@ -10,6 +10,7 @@ public class MuDetect {
     private final InstanceFinder instanceFinder;
     private final ViolationFactory violationFactory;
     private final ViolationRankingStrategy rankingStrategy;
+    private final AlternativePatternInstancePredicate alternativePatternInstancePredicate;
 
     public MuDetect(Model model,
                     InstanceFinder instanceFinder,
@@ -19,10 +20,13 @@ public class MuDetect {
         this.instanceFinder = instanceFinder;
         this.violationFactory = violationFactory;
         this.rankingStrategy = rankingStrategy;
+        // TODO find a testing strategy for this filtering
+        this.alternativePatternInstancePredicate = new AlternativePatternInstancePredicate();
     }
 
     public List<Violation> findViolations(Collection<AUG> targets) {
         final Overlaps overlaps = findOverlaps(targets, model.getPatterns());
+        overlaps.removeViolationIf(violation -> isAlternativePatternInstance(violation, overlaps));
         return rankingStrategy.rankViolations(overlaps, model);
     }
 
@@ -40,6 +44,11 @@ public class MuDetect {
             }
         }
         return overlaps;
+    }
+
+    private boolean isAlternativePatternInstance(Instance violation, Overlaps overlaps) {
+        Set<Instance> instancesInViolationTarget = overlaps.getInstancesInSameTarget(violation);
+        return alternativePatternInstancePredicate.test(violation, instancesInViolationTarget);
     }
 }
 
