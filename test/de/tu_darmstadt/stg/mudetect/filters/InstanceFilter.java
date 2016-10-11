5ccf376b8df42f947d922206e3a74ccf8b942995
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-10-11 10:50:37 +0200

    Remove obsolete code.

--- test/de/tu_darmstadt/stg/mudetect/filters/InstanceFilter.java
+++
@@ -1,8 +0,0 @@
-package de.tu_darmstadt.stg.mudetect.filters;
-
-import de.tu_darmstadt.stg.mudetect.model.Instance;
-import de.tu_darmstadt.stg.mudetect.model.Instances;
-
-import java.util.function.BiPredicate;
-
-public interface InstanceFilter extends BiPredicate<Instance, Instances> {}
