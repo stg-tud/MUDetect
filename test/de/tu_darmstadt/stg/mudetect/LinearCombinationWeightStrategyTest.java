0196b2690fb3547de454f7b37dc5912a14033dfb
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-10-14 15:51:51 +0200

    Linearly combine a single weight function.

---
+++ test/de/tu_darmstadt/stg/mudetect/LinearCombinationWeightStrategyTest.java
@@ -0,0 +1,41 @@
+package de.tu_darmstadt.stg.mudetect;
+
+import de.tu_darmstadt.stg.mudetect.model.Instance;
+import de.tu_darmstadt.stg.mudetect.model.Overlaps;
+import org.junit.Test;
+
+import static de.tu_darmstadt.stg.mudetect.model.TestInstanceBuilder.someInstance;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static utils.SetUtils.asSet;
+
+public class LinearCombinationWeightStrategyTest {
+    @Test
+    public void combinesSingleWeight() throws Exception {
+        Instance instance = someInstance();
+        Overlaps overlaps = new Overlaps();
+        Model model = () -> asSet(instance.getPattern());
+        ViolationWeightStrategy strategy = new LinearCombinationWeightStrategy((v, os, m) -> 42f);
+
+        float weight = strategy.getWeight(instance, overlaps, model);
+
+        assertThat(weight, is(42f));
+    }
+
+    private class LinearCombinationWeightStrategy implements ViolationWeightStrategy {
+        private final ViolationWeightStrategy[] strategies;
+
+        public LinearCombinationWeightStrategy(ViolationWeightStrategy... strategies) {
+            this.strategies = strategies;
+        }
+
+        @Override
+        public float getWeight(Instance violation, Overlaps overlaps, Model model) {
+            float weight = 0;
+            for (ViolationWeightStrategy strategy : strategies) {
+                weight = strategy.getWeight(violation, overlaps, model);
+            }
+            return weight;
+        }
+    }
+}
