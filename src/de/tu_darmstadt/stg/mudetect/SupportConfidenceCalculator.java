609d210b0baa97bb6d1dee6eab9710c36bbcfc6f
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-10-14 15:35:20 +0200

    Make similar signatures more similar.

--- src/de/tu_darmstadt/stg/mudetect/SupportConfidenceCalculator.java
+++ src/de/tu_darmstadt/stg/mudetect/SupportConfidenceCalculator.java
@@ -20,12 +20,13 @@ public class SupportConfidenceCalculator implements ConfidenceCalculator {
 
     @Override
     public float getConfidence(Instance violation, Overlaps overlaps, Model model) {
-        return patternSupportWeightFactor * getPatternSupportWeight(violation.getPattern(), model) +
+        return patternSupportWeightFactor * getPatternSupportWeight(violation, model) +
                 overlapSizeWeightFactor * getOverlapWeight(violation) +
                 violationSupportWeightFactor * getViolationSupportWeight(violation, overlaps);
     }
 
-    private float getPatternSupportWeight(Pattern pattern, Model model) {
+    private float getPatternSupportWeight(Instance violation, Model model) {
+        Pattern pattern = violation.getPattern();
         return pattern.getSupport() / (float) model.getMaxPatternSupport(pattern.getNodeSize());
     }
 
