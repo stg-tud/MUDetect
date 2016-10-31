0b25ef55185d70e00cf4fcab89bd4ff2be45e294
nguyen_hoan <hoan@iastate.edu>
2016-10-31 12:30:37 -0500

    add test case parsing jdt core code

--- test/typeresolution/BatchParser.java
+++ test/typeresolution/BatchParser.java
@@ -38,7 +38,7 @@ import utils.FileIO;
 public class BatchParser {
 	private static ASTParser parser;
 	private static HashMap<String, ASTNode> asts = new HashMap<>();
-	private static int mismatches = 0;
+	private static int mismatches = 0, mismatchesInOnline = 0, mismatchesInBatch = 0;
 	
 	@Rule
 	public TestName testName = new TestName();
@@ -46,10 +46,23 @@ public class BatchParser {
 	@Test
 	public void testReuseParser() {
 		ArrayList<File> files = getPaths(new String[] {"src"});
-		parse(files);
-		parse(files);
+//		String classPath = "D:/Projects/GROUMiner/target/classes";
+		String classPath = "D:/Projects/aug-miner.jar";
+		parse(files, classPath);
+		parse(files, classPath);
 		assertThat(mismatches, is(0));
-		parseBatch(files);
+		parseBatch(files, classPath);
+		assertThat(mismatches, is(0));
+	}
+
+	@Test
+	public void testReuseParserJDT() {
+		ArrayList<File> files = getPaths(new String[] {"C:/Users/hoan/.m2/repository/org/eclipse/tycho/org.eclipse.jdt.core/3.11.1.v20150902-1521/org.eclipse.jdt.core-3.11.1.v20150902-1521-sources"});
+		String classPath = "C:/Users/hoan/.m2/repository/org/eclipse/tycho/org.eclipse.jdt.core/3.11.1.v20150902-1521/org.eclipse.jdt.core-3.11.1.v20150902-1521.jar";
+		parse(files, classPath);
+		parse(files, classPath);
+		assertThat(mismatches, is(0));
+		parseBatch(files, classPath);
 		assertThat(mismatches, is(0));
 	}
 
@@ -70,7 +83,7 @@ public class BatchParser {
 		return files;
 	}
 
-	private void parseBatch(ArrayList<File> files) {
+	private void parseBatch(ArrayList<File> files, String classPath) {
 		long start = System.currentTimeMillis();
 		mismatches = 0;
 		String[] paths = new String[files.size()];
@@ -84,7 +97,7 @@ public class BatchParser {
 				cus.put(sourceFilePath, ast);
 			}
 		};
-		init();
+		init(classPath);
 		parser.createASTs(paths, null, new String[0], r, null);
 		for (String path : cus.keySet()) {
 			ASTNode ast = asts.get(path);
@@ -93,9 +106,17 @@ public class BatchParser {
 				@Override
 				public boolean match(MethodInvocation node, Object other) {
 					if (super.match(node, other)) {
-						if (node.resolveMethodBinding() == null)
-							return false;
 						MethodInvocation mi = (MethodInvocation) other;
+						if (node.resolveMethodBinding() == null) {
+							if (mi.resolveMethodBinding() == null)
+								return true;
+							mismatchesInOnline++;
+							return false;
+						}
+						if (mi.resolveMethodBinding() == null) {
+							mismatchesInBatch++;
+							return false;
+						}
 						if (node.resolveMethodBinding().isEqualTo(mi.resolveMethodBinding()))
 							return true;
 						return false;
@@ -105,20 +126,22 @@ public class BatchParser {
 			}, node)) {
 				mismatches++;
 				//throw new RuntimeException();
-				System.err.println("Not matched!!! " + mismatches);
+//				System.err.println("Not matched!!! " + mismatches + " / " + files.size());
 			}
 		}
 		long end = System.currentTimeMillis();
-		System.out.println("Batch: " + (end - start));
+		System.out.println("Batch: " + mismatches + " / " + files.size() + " in " + (end - start) + "ms");
+		System.out.println("mismatchesInBatch: " + mismatchesInBatch);
+		System.out.println("mismatchesInOnline: " + mismatchesInOnline);
 	}
 
-	public void parse(ArrayList<File> files) {
+	public void parse(ArrayList<File> files, String classPath) {
 		long start = System.currentTimeMillis();
 		mismatches = 0;
 		for (File file : files) {
 			String name = file.getName();
 			String source = FileIO.readStringFromFile(file.getAbsolutePath());
-			init();
+			init(classPath);
 			parser.setSource(source.toCharArray());
 			parser.setUnitName(name);
 			ASTNode node = parser.createAST(null);
@@ -130,9 +153,12 @@ public class BatchParser {
 					@Override
 					public boolean match(MethodInvocation node, Object other) {
 						if (super.match(node, other)) {
-							if (node.resolveMethodBinding() == null)
-								return false;
 							MethodInvocation mi = (MethodInvocation) other;
+							if (node.resolveMethodBinding() == null) {
+								if (mi.resolveMethodBinding() == null)
+									return true;
+								return false;
+							}
 							if (node.resolveMethodBinding().isEqualTo(mi.resolveMethodBinding()))
 								return true;
 							return false;
@@ -142,14 +168,14 @@ public class BatchParser {
 				}, node)) {
 					mismatches++;
 					//throw new RuntimeException();
-					System.err.println("Not matched!!! " + mismatches);
+					System.err.println("Not matched!!! " + mismatches + " / " + files.size());
 				}
 		}
 		long end = System.currentTimeMillis();
-		System.out.println("Online: " + (end - start));
+		System.out.println("Online: " + mismatches + " / " + files.size() + " in " + (end - start) + "ms");
 	}
 
-	public void init() {
+	public void init(String classPath) {
 		@SuppressWarnings("rawtypes")
 		Map options = JavaCore.getOptions();
 		options.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_8);
@@ -158,7 +184,7 @@ public class BatchParser {
 		parser = ASTParser.newParser(AST.JLS8);
 		parser.setCompilerOptions(options);
 		parser.setEnvironment(
-				new String[]{"D:/Projects/aug-miner.jar"}, 
+				new String[]{classPath}, 
 				new String[]{}, 
 				new String[]{}, 
 				true);
