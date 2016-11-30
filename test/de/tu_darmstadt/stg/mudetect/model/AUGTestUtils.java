008bde4ce7d5c7691d793d428829dfb45c97884f
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-11-30 13:38:43 +0100

    Test encoding of conditions.

--- test/de/tu_darmstadt/stg/mudetect/model/AUGTestUtils.java
+++ test/de/tu_darmstadt/stg/mudetect/model/AUGTestUtils.java
@@ -1,15 +1,17 @@
 package de.tu_darmstadt.stg.mudetect.model;
 
-import egroum.EGroumDataNode;
-import egroum.EGroumEdge;
-import egroum.EGroumNode;
+import egroum.*;
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static org.hamcrest.core.AllOf.allOf;
 
 public class AUGTestUtils {
     public static Matcher<AUG> isEqual(AUG expected) {
@@ -75,6 +77,32 @@ public class AUGTestUtils {
         };
     }
 
+    @SafeVarargs
+    public static Matcher<? super AUG> hasNodes(Matcher<? super EGroumNode>... matchers) {
+        List<Matcher<? super AUG>> all = new ArrayList<>(matchers.length);
+
+        for (Matcher<? super EGroumNode> matcher : matchers) {
+            all.add(hasNode(matcher));
+        }
+
+        return allOf(all);
+    }
+
+    public static Matcher<? super EGroumNode> actionNodeWithLabel(String label) {
+        return new BaseMatcher<EGroumNode>() {
+            @Override
+            public boolean matches(Object item) {
+                return item instanceof EGroumActionNode && ((EGroumActionNode) item).getLabel().equals(label);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("an action node with label ");
+                description.appendValue(label);
+            }
+        };
+    }
+
     public static Matcher<? super EGroumNode> dataNodeWithLabel(String label) {
         return new BaseMatcher<EGroumNode>() {
             @Override
@@ -88,5 +116,66 @@ public class AUGTestUtils {
                 description.appendValue(label);
             }
         };
+    }
+
+    public static Matcher<? super AUG> hasSelEdge(Matcher<? super EGroumNode> sourceMatcher,
+                                                  Matcher<? super EGroumNode> targetMatcher) {
+        return hasEdge(new EdgeMatcher(sourceMatcher, "sel", targetMatcher));
+    }
+
+    public static Matcher<? super AUG> hasEdge(final Matcher<? super EGroumNode> sourceMatcher,
+                                               final EGroumDataEdge.Type edgeType,
+                                               final Matcher<? super EGroumNode> targetMatcher) {
+        return hasEdge(new EdgeMatcher(sourceMatcher, EGroumDataEdge.getLabel(edgeType), targetMatcher));
+    }
+
+    private static Matcher<? super AUG> hasEdge(Matcher<? super EGroumEdge> matcher) {
+        return new BaseMatcher<AUG>() {
+            @Override
+            public boolean matches(Object item) {
+                if (item instanceof AUG) {
+                    Set<EGroumEdge> edges = ((AUG) item).edgeSet();
+                    return Matchers.hasItem(matcher).matches(edges);
+                }
+                return false;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("an AUG containing ");
+                description.appendDescriptionOf(matcher);
+            }
+        };
+    }
+
+    private static class EdgeMatcher extends BaseMatcher<EGroumEdge> {
+        private final Matcher<? super EGroumNode> sourceMatcher;
+        private final String edgeLabel;
+        private final Matcher<? super EGroumNode> targetMatcher;
+
+        private EdgeMatcher(Matcher<? super EGroumNode> sourceMatcher,
+                            String edgeLabel,
+                            Matcher<? super EGroumNode> targetMatcher) {
+            this.sourceMatcher = sourceMatcher;
+            this.edgeLabel = edgeLabel;
+            this.targetMatcher = targetMatcher;
+        }
+
+        @Override
+        public boolean matches(Object item) {
+            if (item instanceof EGroumDataEdge) {
+                EGroumDataEdge edge = (EGroumDataEdge) item;
+                return sourceMatcher.matches(edge.getSource())
+                        && edge.getLabel().equals(edgeLabel)
+                        && targetMatcher.matches(edge.getTarget());
+            }
+            return false;
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("an edge with label ").appendValue(edgeLabel).appendText(" from ");
+            description.appendDescriptionOf(sourceMatcher).appendText(" to ").appendDescriptionOf(targetMatcher);
+        }
     }
 }
