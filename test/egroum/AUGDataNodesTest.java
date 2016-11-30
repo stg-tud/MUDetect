32c79b11d09c8400a2252b5e48f1fa5ed4489737
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-11-30 11:26:20 +0100

    Test builder adds no data nodes for unused objects.

--- test/egroum/AUGDataNodesTest.java
+++ test/egroum/AUGDataNodesTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import java.util.Set;
 
 import static egroum.EGroumTestUtils.buildGroumForMethod;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 public class AUGDataNodesTest {
@@ -32,6 +33,27 @@ public class AUGDataNodesTest {
         AUG aug = buildAUG("String m() { return toString(); }");
 
         assertThat(aug, hasNode(dataNodeWithLabel("String")));
+    }
+
+    @Test
+    public void noDataNodeForUnusedReturnValue() throws Exception {
+        AUG aug = buildAUG("void m() { toString(); }");
+
+        assertThat(aug, not(hasNode(dataNodeWithLabel("String"))));
+    }
+
+    @Test
+    public void noDataNodeForUnusedVariable() throws Exception {
+        AUG aug = buildAUG("void m() { String s = toString(); }");
+
+        assertThat(aug, not(hasNode(dataNodeWithLabel("String"))));
+    }
+
+    @Test
+    public void noDataNodeForUnusedLiteral() throws Exception {
+        AUG aug = buildAUG("void m() { String s = \"foo\"; }");
+
+        assertThat(aug, not(hasNode(dataNodeWithLabel("String"))));
     }
 
     private Matcher<? super AUG> hasNode(Matcher<? super EGroumNode> matcher) {
