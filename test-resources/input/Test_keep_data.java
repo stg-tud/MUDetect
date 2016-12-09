568ce0b1ba5560a27e17a4fc3c041f69d29c9446
nguyen_hoan <hoan@iastate.edu>
2016-12-08 23:49:48 -0600

    recover test case input

--- test-resources/input/Test_keep_data.java
+++ test-resources/input/Test_keep_data.java
@@ -9,19 +9,19 @@ class Test_keep_data {
 	
 	String s;
 	
-//	public void before() {
-//		StringBuffer sb = new StringBuffer();
-//		sb.append("");
-//		sb.append(s.length());
-//		sb.append("");
-//		sb.toString();
-//	}
-//
-//	public void after(String s) {
-//		StringBuffer sb = new StringBuffer();
-//		sb.append(s);
-//		sb.toString();
-//	}
+	public void before() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("");
+		sb.append(s.length());
+		sb.append("");
+		sb.toString();
+	}
+
+	public void after(String s) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(s);
+		sb.toString();
+	}
 	
 	void m() {
 		a[0] = a[0] + 1;
