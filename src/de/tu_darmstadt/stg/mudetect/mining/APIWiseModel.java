94150c19638951031ef40bbc6bca6b1c5eb2e89c
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-03-24 15:56:29 +0100

    Group patterns by API after mining.

--- src/de/tu_darmstadt/stg/mudetect/mining/APIWiseModel.java
+++ src/de/tu_darmstadt/stg/mudetect/mining/APIWiseModel.java
@@ -1,67 +1,34 @@
 package de.tu_darmstadt.stg.mudetect.mining;
 
-import egroum.EGroumDataNode;
 import egroum.EGroumGraph;
-import egroum.EGroumNode;
 import mining.Configuration;
 
 import java.util.*;
 
 class APIWiseModel {
-    private Map<String, Model> modelByAPI = new HashMap<>();
+
+    private final Map<String, Set<Pattern>> apiToPatternMapping;
 
     public APIWiseModel(Configuration configuration, Collection<EGroumGraph> groums) {
-        Map<String, Collection<EGroumGraph>> apiToUsageMapping = getAPItoUsageMapping(groums);
-        for (String api : apiToUsageMapping.keySet()) {
-            Collection<EGroumGraph> examples = apiToUsageMapping.get(api);
-            MinedPatternsModel apiModel = new MinedPatternsModel(configuration, examples);
-            if (!apiModel.getPatterns().isEmpty()) {
-                modelByAPI.put(api, apiModel);
-            }
-        }
+        Set<Pattern> patterns = new MinedPatternsModel(configuration, groums).getPatterns();
+        apiToPatternMapping = getAPItoPatternMapping(patterns);
     }
 
     public Set<String> getAPIs() {
-        return modelByAPI.keySet();
+        return apiToPatternMapping.keySet();
     }
 
-    private static Map<String, Collection<EGroumGraph>> getAPItoUsageMapping(Collection<EGroumGraph> groums) {
-        Map<String, Collection<EGroumGraph>> apiToUsageMapping = new HashMap<>();
-        for (EGroumGraph groum : groums) {
-            Set<String> apis = getAPIs(groum);
+    private static Map<String, Set<Pattern>> getAPItoPatternMapping(Set<Pattern> patterns) {
+        Map<String, Set<Pattern>> apiToUsageMapping = new HashMap<>();
+        for (Pattern pattern : patterns) {
+            Set<String> apis = pattern.getAPIs();
             for (String api : apis) {
                 if (!apiToUsageMapping.containsKey(api)) {
                     apiToUsageMapping.put(api, new HashSet<>());
                 }
-                apiToUsageMapping.get(api).add(groum);
+                apiToUsageMapping.get(api).add(pattern);
             }
         }
         return apiToUsageMapping;
-    }
-
-    private static Set<String> getAPIs(EGroumGraph groum) {
-        Set<String> apis = new HashSet<>();
-        for (EGroumNode node : groum.getNodes()) {
-            if (isAPI(node)) apis.add(node.getLabel());
-        }
-        return apis;
-    }
-
-    private static boolean isAPI(EGroumNode node) {
-        if (node instanceof EGroumDataNode) {
-            String label = node.getLabel();
-            switch (label) {
-                case "int":
-                case "long":
-                case "float":
-                case "double":
-                case "short":
-                case "boolean":
-                    return false;
-                default:
-                    return !label.endsWith("[]");
-            }
-        }
-        return false;
     }
 }
