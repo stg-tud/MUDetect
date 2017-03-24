1048f6da4aa96f177f3ebd3971110d9d6ee24c89
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-03-24 16:50:33 +0100

    Count examples of API before mining.

--- src/de/tu_darmstadt/stg/mudetect/mining/APIWiseModel.java
+++ src/de/tu_darmstadt/stg/mudetect/mining/APIWiseModel.java
@@ -1,21 +1,44 @@
 package de.tu_darmstadt.stg.mudetect.mining;
 
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
 import egroum.EGroumGraph;
+import egroum.EGroumNode;
 import mining.Configuration;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 class APIWiseModel {
 
+    private final Multiset<String> apiOccurrences;
     private final Map<String, Set<Pattern>> apiToPatternMapping;
 
     public APIWiseModel(Configuration configuration, Collection<EGroumGraph> groums) {
+        apiOccurrences = getAPIOccurrences(groums);
         Set<Pattern> patterns = new MinedPatternsModel(configuration, groums).getPatterns();
         apiToPatternMapping = getAPItoPatternMapping(patterns);
     }
 
+    public Set<String> getExampleAPIs() {
+        return apiOccurrences.elementSet();
+    }
+
+    public int getNumberOfExamples(String api) {
+        return apiOccurrences.count(api);
+    }
+
     public Set<String> getPatternAPIs() {
         return apiToPatternMapping.keySet();
+    }
+
+    private Multiset<String> getAPIOccurrences(Collection<EGroumGraph> groums) {
+        Multiset<String> apiOccurrences = HashMultiset.create();
+        for (EGroumGraph groum : groums) {
+            apiOccurrences.addAll(groum.getNodes().stream()
+                    .filter(EGroumNode::isAPI).map(EGroumNode::getLabel).collect(Collectors.toList()));
+        }
+        return apiOccurrences;
     }
 
     private static Map<String, Set<Pattern>> getAPItoPatternMapping(Set<Pattern> patterns) {
--- test/de/tu_darmstadt/stg/mudetect/mining/APIWiseModelTest.java
+++ test/de/tu_darmstadt/stg/mudetect/mining/APIWiseModelTest.java
@@ -7,9 +7,7 @@ import org.junit.Test;
 import java.util.*;
 
 import static egroum.EGroumTestUtils.buildGroumForMethod;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 import static utils.SetUtils.asSet;
 
@@ -38,5 +36,19 @@ public class APIWiseModelTest {
         APIWiseModel model = new APIWiseModel(new Configuration() {{ minPatternSupport = 2; }}, groums);
 
         assertThat(model.getPatternAPIs(), not(hasItem("Object")));
+    }
+
+    @Test
+    public void countsNumberOfExamples() throws Exception {
+        Set<EGroumGraph> groums = asSet(
+                buildGroumForMethod("void m(A a) { a.m(); }"),
+                buildGroumForMethod("void n(A a) { a.n(); }"),
+                buildGroumForMethod("void o(B b) { b.m(); }"));
+
+        APIWiseModel model = new APIWiseModel(new Configuration() {{ minPatternSupport = 2; }}, groums);
+
+        assertThat(model.getExampleAPIs(), containsInAnyOrder("A", "B"));
+        assertThat(model.getNumberOfExamples("A"), is(2));
+        assertThat(model.getNumberOfExamples("B"), is(1));
     }
 }
