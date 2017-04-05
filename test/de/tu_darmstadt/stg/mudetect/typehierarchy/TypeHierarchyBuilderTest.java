87558e961ee313f03452eb40ed28b1010cb72944
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-04-05 14:10:05 +0200

    Add hierarchy test cases.

--- test/de/tu_darmstadt/stg/mudetect/typehierarchy/TypeHierarchyBuilderTest.java
+++ test/de/tu_darmstadt/stg/mudetect/typehierarchy/TypeHierarchyBuilderTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class TypeHierarchyBuilderTest {
     @Test
@@ -14,16 +13,15 @@ public class TypeHierarchyBuilderTest {
 
         TypeHierarchy hierarchy = builder.build(new String[] {TypeHierarchyBuilder.RT_JAR_PATH});
 
-        assertTrue(hierarchy.isA("List", "Collection"));
-    }
-
-    @Test
-    public void ignoresAnonymousTypes() throws Exception {
-        TypeHierarchyBuilder builder = new TypeHierarchyBuilder();
-
-        TypeHierarchy hierarchy = builder.build(new String[] {TypeHierarchyBuilder.RT_JAR_PATH});
-
-        assertFalse(hierarchy.isA("1", "Object"));
+        assertTrue(hierarchy.isA("ArrayList", "AbstractList"));       // extends
+        assertTrue(hierarchy.isA("ArrayList", "AbstractCollection")); // extends transitively
+        assertTrue(hierarchy.isA("ArrayList", "Cloneable"));          // implements
+        assertTrue(hierarchy.isA("ArrayList", "List"));               // implements transitively
+        assertTrue(hierarchy.isA("List", "Collection"));              // interface extends
+        assertTrue(hierarchy.isA("List", "Iterable"));                // interface extends transitively
+        
+        assertFalse(hierarchy.isA("1", "Object"));                    // excludes anonymous types
+        assertFalse(hierarchy.isA("List", "Set"));                    // non-existent relation
     }
 
     @Test @Ignore("Only works after running 'mvn package'")
