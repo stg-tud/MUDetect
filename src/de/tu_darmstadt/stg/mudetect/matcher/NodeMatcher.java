0bf4c2230f2a77fdbd47366b0ca8212f8525af0e
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-04-11 16:30:21 +0200

    Document interface.

--- src/de/tu_darmstadt/stg/mudetect/matcher/NodeMatcher.java
+++ src/de/tu_darmstadt/stg/mudetect/matcher/NodeMatcher.java
@@ -4,4 +4,12 @@ import egroum.EGroumNode;
 
 import java.util.function.BiPredicate;
 
-public interface NodeMatcher extends BiPredicate<EGroumNode, EGroumNode> {}
+public interface NodeMatcher extends BiPredicate<EGroumNode, EGroumNode> {
+    /**
+     * @param targetNode the target node to match
+     * @param patternNode the pattern node to match against
+     * @return whether the target node matches the pattern node (need not be symmetric!)
+     */
+    @Override
+    boolean test(EGroumNode targetNode, EGroumNode patternNode);
+}
