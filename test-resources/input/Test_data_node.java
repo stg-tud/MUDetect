ae21a6ebdbea02c84fcd98c8ef77c68ca348793c
nguyen_hoan <hoan@iastate.edu>
2017-04-18 22:43:10 -0500

    update test case for data node from field

--- test-resources/input/Test_data_node.java
+++ test-resources/input/Test_data_node.java
@@ -1,29 +1,41 @@
 package input;
 
 class Test_data_node extends BufferedIndexOutput {
-	
-	  void pattern(InputStream in) throws IOException {
-	      byte[] spoolBuffer = new byte[0x2000];
-	      int read;
-	      try {
-	        while ((read = in.read(spoolBuffer)) > 0) {
-	          // do something...
-	        }
-	      } finally {
-	          in.close();
-	      }
-	  }
-	  
-	  int v1, v2, v3, v4;
-	  void m() {
-		  v1 = m1();
-		  v2 = m2(v1);
-		  v3 = m3(v1);
-		  m4(v2, v3);
-		  v1 = m1();
-		  v2 = m2(v1);
-		  v3 = m3(v1);
-		  m4(v2, v3);
-		  m4(v2, v3);
-	  }
+
+	void apply() {
+		if (NodeUtil.isFunctionDeclaration(node)) {
+			traverseFunction(node, scope);
+		} else {
+			for (Node child = node.getFirstChild();
+					child != null; child = child.getNext()) {
+				traverseNode(child, node, scope);
+			}
+		}
+	}
+
+
+	void pattern(InputStream in) throws IOException {
+		byte[] spoolBuffer = new byte[0x2000];
+		int read;
+		try {
+			while ((read = in.read(spoolBuffer)) > 0) {
+				// do something...
+			}
+		} finally {
+			in.close();
+		}
+	}
+
+	int v1, v2, v3, v4;
+	void m() {
+		v1 = m1();
+		v2 = m2(v1);
+		v3 = m3(v1);
+		m4(v2, v3);
+		v1 = m1();
+		v2 = m2(v1);
+		v3 = m3(v1);
+		m4(v2, v3);
+		m4(v2, v3);
+	}
 }
