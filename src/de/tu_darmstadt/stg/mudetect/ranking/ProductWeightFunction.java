b0dd481b00620cad999afa24f89f61df5d3182ab
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-04-21 17:08:11 +0200

    Add product weight function.

---
+++ src/de/tu_darmstadt/stg/mudetect/ranking/ProductWeightFunction.java
@@ -0,0 +1,30 @@
+package de.tu_darmstadt.stg.mudetect.ranking;
+
+import de.tu_darmstadt.stg.mudetect.mining.Model;
+import de.tu_darmstadt.stg.mudetect.model.Overlap;
+import de.tu_darmstadt.stg.mudetect.model.Overlaps;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ProductWeightFunction implements ViolationWeightFunction {
+    private final ViolationWeightFunction[] factors;
+
+    public ProductWeightFunction(ViolationWeightFunction... factors) {
+        this.factors = factors;
+    }
+
+    @Override
+    public double getWeight(Overlap violation, Overlaps overlaps, Model model) {
+        return Arrays.stream(factors)
+                .mapToDouble(factor -> factor.getWeight(violation, overlaps, model))
+                .reduce(1, (a, b) -> a * b);
+    }
+
+    @Override
+    public String getFormula(Overlap violation, Overlaps overlaps, Model model) {
+        return Arrays.stream(factors)
+                .map(factor -> factor.getFormula(violation, overlaps, model))
+                .collect(Collectors.joining(")*(", "(", ")"));
+    }
+}
---
+++ test/de/tu_darmstadt/stg/mudetect/ranking/ProductWeightFunctionTest.java
@@ -0,0 +1,43 @@
+package de.tu_darmstadt.stg.mudetect.ranking;
+
+import de.tu_darmstadt.stg.mudetect.mining.Model;
+import de.tu_darmstadt.stg.mudetect.model.Overlap;
+import de.tu_darmstadt.stg.mudetect.model.Overlaps;
+import org.junit.Test;
+
+import static de.tu_darmstadt.stg.mudetect.model.TestOverlapBuilder.someOverlap;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static utils.SetUtils.asSet;
+
+public class ProductWeightFunctionTest {
+    @Test
+    public void multipliesWeights() throws Exception {
+        Overlap instance = someOverlap();
+        Overlaps overlaps = new Overlaps();
+        Model model = () -> asSet(instance.getPattern());
+
+        ProductWeightFunction weightFunction = new ProductWeightFunction(w((o, os, m) -> 0.5), w((o, os, m) -> 0.5));
+
+        assertThat(weightFunction.getWeight(instance, overlaps, model), is(0.25));
+    }
+
+    private static ViolationWeightFunction w(AverageWeightFunctionTest.Function<Overlap, Overlaps, Model, Double> weight) {
+        return new ViolationWeightFunction() {
+            @Override
+            public double getWeight(Overlap violation, Overlaps overlaps, Model model) {
+                return weight.apply(violation, overlaps, model);
+            }
+
+            @Override
+            public String getFormula(Overlap violation, Overlaps overlaps, Model model) {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    @FunctionalInterface
+    interface Function<A, B, C, R> {
+        R apply(A a, B b, C c);
+    }
+}
