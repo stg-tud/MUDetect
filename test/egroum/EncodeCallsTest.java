f31c40b239de2e9024f5929ca4af3af2745a79fa
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-04-24 14:00:07 +0200

    Add tests for receiver and parameter edges.

--- test/egroum/EncodeCallsTest.java
+++ test/egroum/EncodeCallsTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 
 import static de.tu_darmstadt.stg.mudetect.model.AUGTestUtils.*;
 import static egroum.AUGBuilderTestUtils.buildAUG;
+import static egroum.EGroumDataEdge.Type.PARAMETER;
 import static egroum.EGroumDataEdge.Type.RECEIVER;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 public class EncodeCallsTest {
@@ -32,5 +34,50 @@ public class EncodeCallsTest {
         AUG aug = buildAUG("void m() { new Object(); }");
 
         assertThat(aug, hasNode(actionNodeWithLabel("Object.<init>")));
+    }
+
+    @Test
+    public void encodesOnlyDirectReceiverEdges() throws Exception {
+        AUG aug = buildAUG("void m(java.io.File f) {\n" +
+                "  java.io.InputStream is = new java.io.FileInputStream(f);\n" +
+                "  is.read();\n" +
+                "}");
+
+        assertThat(aug, hasEdge(actionNodeWithLabel("FileInputStream.<init>"), RECEIVER, actionNodeWithLabel("InputStream.read()")));
+        assertThat(aug, hasEdge(dataNodeWithLabel("InputStream"), RECEIVER, actionNodeWithLabel("InputStream.read()")));
+        assertThat(aug, not(hasEdge(dataNodeWithLabel("File"), RECEIVER, actionNodeWithLabel("InputStream.read()"))));
+    }
+
+    @Test
+    public void encodesOnlyDirectParameterEdges() throws Exception {
+        AUG aug = buildAUG("void m(java.io.File f) {\n" +
+                "  java.io.InputStream is = new java.io.FileInputStream(f);\n" +
+                "  java.io.Reader r = new InputStreamReader(is);\n" +
+                "}");
+
+        assertThat(aug, hasEdge(dataNodeWithLabel("File"), PARAMETER, actionNodeWithLabel("FileInputStream.<init>")));
+        assertThat(aug, hasEdge(actionNodeWithLabel("FileInputStream.<init>"), PARAMETER, actionNodeWithLabel("InputStreamReader.<init>")));
+        assertThat(aug, hasEdge(dataNodeWithLabel("InputStream"), PARAMETER, actionNodeWithLabel("InputStreamReader.<init>")));
+        assertThat(aug, not(hasEdge(dataNodeWithLabel("File"), PARAMETER, actionNodeWithLabel("InputStreamReader.<init>"))));
+    }
+
+    @Test
+    public void encodesTransitiveParameterEdgesThroughArithmeticOperators() throws Exception {
+        AUG aug = buildAUG("Object m(java.util.List l) {\n" +
+                "  return l.get(l.size() - 1);\n" +
+                "}");
+
+        assertThat(aug, hasEdge(actionNodeWithLabel("Collection.size()"), PARAMETER, actionNodeWithLabel("List.get()")));
+        assertThat(aug, hasEdge(dataNodeWithLabel("int"), PARAMETER, actionNodeWithLabel("List.get()")));
+    }
+
+    @Test
+    public void encodesTransitiveParameterEdgesThroughBooleanOperators() throws Exception {
+        AUG aug = buildAUG("boolean m(java.util.List l) {\n" +
+                "  return !l.isEmpty();\n" +
+                "}");
+
+        assertThat(aug, hasEdge(actionNodeWithLabel("Collection.isEmpty()"), PARAMETER, actionNodeWithLabel("return")));
+        assertThat(aug, hasEdge(dataNodeWithLabel("boolean"), PARAMETER, actionNodeWithLabel("return")));
     }
 }
