ce0ba97b22f2e5c7b24283f63cb532820bbdb9ad
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-04-25 12:18:53 +0200

    Add some detetion integration tests.

--- test/de/tu_darmstadt/stg/mudetect/AlternativePatternsIntegrationTest.java
+++ test/de/tu_darmstadt/stg/mudetect/AlternativePatternsIntegrationTest.java
@@ -5,6 +5,7 @@ import de.tu_darmstadt.stg.mudetect.matcher.SubtypeDataNodeMatcher;
 import de.tu_darmstadt.stg.mudetect.model.AUG;
 import de.tu_darmstadt.stg.mudetect.model.Overlap;
 import de.tu_darmstadt.stg.mudetect.mining.Pattern;
+import de.tu_darmstadt.stg.mudetect.model.TestAUGBuilder;
 import de.tu_darmstadt.stg.mudetect.typehierarchy.TypeHierarchy;
 import de.tu_darmstadt.stg.mudetect.model.Violation;
 import de.tu_darmstadt.stg.mudetect.ranking.NoRankingStrategy;
@@ -18,6 +19,9 @@ import java.util.List;
 import static de.tu_darmstadt.stg.mudetect.model.TestOverlapBuilder.buildOverlap;
 import static de.tu_darmstadt.stg.mudetect.mining.TestPatternBuilder.somePattern;
 import static egroum.AUGBuilderTestUtils.buildAUG;
+import static egroum.EGroumDataEdge.Type.DEFINITION;
+import static egroum.EGroumDataEdge.Type.PARAMETER;
+import static egroum.EGroumDataEdge.Type.RECEIVER;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
@@ -40,7 +44,7 @@ public class AlternativePatternsIntegrationTest {
 
         Overlap overlap = buildOverlap(patternA, target)
                 .withNodes("Iterator", "Iterator.hasNext()")
-                .withEdge("Iterator", EGroumDataEdge.Type.RECEIVER, "Iterator.hasNext()").build();
+                .withEdge("Iterator", RECEIVER, "Iterator.hasNext()").build();
         Violation violation = new Violation(overlap, 1.0f, "(pattern support = 3) / (max pattern support = 3)");
         assertThat(violations, contains(violation));
     }
@@ -57,6 +61,82 @@ public class AlternativePatternsIntegrationTest {
 
         List<Violation> violations = detector.findViolations(asSet(target));
 
+        assertThat(violations, is(empty()));
+    }
+
+    @Test
+    public void multipleCallViolations() throws Exception {
+        Pattern pattern = somePattern(TestAUGBuilder.buildAUG().withActionNode("JPanel.<init>").withDataNode("JPanel")
+                .withActionNode("add1", "JPanel.add()").withActionNode("add2", "JPanel.add()")
+                .withDataEdge("JPanel.<init>", DEFINITION, "JPanel")
+                .withDataEdge("JPanel.<init>", RECEIVER, "add1")
+                .withDataEdge("JPanel", RECEIVER, "add1")
+                .withDataEdge("JPanel.<init>", RECEIVER, "add2")
+                .withDataEdge("JPanel", RECEIVER, "add2")
+                .withDataEdge("add2", EGroumDataEdge.Type.ORDER, "add1").build());
+        AUG target = buildAUG("import javax.swing.JPanel;\n" +
+                "class C {\n" +
+                "  void m() {\n" +
+                "    JPanel controlPanel = new JPanel();\n"+
+                "    controlPanel.add(null);\n"+
+                "    controlPanel.add(null);\n"+
+                "    controlPanel.add(null);\n"+
+                "    controlPanel.add(null);\n" +
+                "  }\n" +
+                "}");
+        MuDetect detector = new MuDetect(() -> asSet(pattern),
+                new AlternativeMappingsOverlapsFinder(new EquallyLabelledNodeMatcher()),
+                new MissingElementViolationFactory(),
+                new NoRankingStrategy());
+
+        List<Violation> violations = detector.findViolations(asSet(target));
+
+        assertThat(violations, is(empty()));
+    }
+
+    @Test
+    public void mappingToWrongDataNode() throws Exception {
+        Pattern pattern = somePattern(TestAUGBuilder.buildAUG()
+                .withDataNode("IO1", "IndexOutput")
+                .withActionNode("gFP()", "IndexOutput.getFilePointer()")
+                .withDataEdge("IO1", RECEIVER, "gFP()")
+                .withDataNode("long")
+                .withDataEdge("gFP()", DEFINITION, "long")
+                .withDataNode("IO2", "IndexOutput")
+                .withActionNode("wL()", "IndexOutput.writeLong()")
+                .withDataEdge("IO2", RECEIVER, "wL()")
+                .withDataEdge("gFP()", PARAMETER, "wL()")
+                .withDataEdge("long", PARAMETER, "wL()"));
+        AUG target = buildAUG("class C {\n" +
+                "  IndexOutput tvd, tvf, tvx;" +
+                "  void addRawDocuments(TermVectorsReader reader, int[] tvdLengths, int[] tvfLengths, int numDocs) throws IOException {\n" +
+                "    long tvdPosition = tvd.getFilePointer();\n" +
+                "    long tvfPosition = tvf.getFilePointer();\n" +
+                "    long tvdStart = tvdPosition;\n" +
+                "    long tvfStart = tvfPosition;\n" +
+                "    for(int i=0;i<numDocs;i++) {\n" +
+                "      tvx.writeLong(tvdPosition);\n" +
+                "      tvdPosition += tvdLengths[i];\n" +
+                "      tvx.writeLong(tvfPosition);\n" +
+                "      tvfPosition += tvfLengths[i];\n" +
+                "    }\n" +
+                "    tvd.copyBytes(reader.getTvdStream(), tvdPosition-tvdStart);\n" +
+                "    tvf.copyBytes(reader.getTvfStream(), tvfPosition-tvfStart);\n" +
+                "    assert tvd.getFilePointer() == tvdPosition;\n" +
+                "    assert tvf.getFilePointer() == tvfPosition;" +
+                "  }\n" +
+                "}");
+
+        MuDetect detector = new MuDetect(() -> asSet(pattern),
+                new AlternativeMappingsOverlapsFinder(new EquallyLabelledNodeMatcher()),
+                new MissingElementViolationFactory(),
+                new NoRankingStrategy());
+
+        List<Violation> violations = detector.findViolations(asSet(target));
+
+        if (!violations.isEmpty()) {
+            System.out.println(violations.get(0).getOverlap().getMissingEdges());
+        }
         assertThat(violations, is(empty()));
     }
 
