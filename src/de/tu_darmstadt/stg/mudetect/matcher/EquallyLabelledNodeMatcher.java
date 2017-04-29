e434a972404e88f904f27ac5d489a46a8bb75280
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-04-29 14:21:53 +0200

    Equally-labelled node matcher accepts label provider.

--- src/de/tu_darmstadt/stg/mudetect/matcher/EquallyLabelledNodeMatcher.java
+++ src/de/tu_darmstadt/stg/mudetect/matcher/EquallyLabelledNodeMatcher.java
@@ -2,9 +2,21 @@ package de.tu_darmstadt.stg.mudetect.matcher;
 
 import egroum.EGroumNode;
 
+import java.util.function.Function;
+
 public class EquallyLabelledNodeMatcher implements NodeMatcher {
+    private final Function<EGroumNode, String> getLabel;
+
+    public EquallyLabelledNodeMatcher() {
+        this(EGroumNode::getLabel);
+    }
+
+    public EquallyLabelledNodeMatcher(Function<EGroumNode, String> getLabel) {
+        this.getLabel = getLabel;
+    }
+
     @Override
     public boolean test(EGroumNode node1, EGroumNode node2) {
-        return node1.getLabel().equals(node2.getLabel());
+        return getLabel.apply(node1).equals(getLabel.apply(node2));
     }
 }
