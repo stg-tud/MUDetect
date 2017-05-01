32128e9b5e9033c5cd139dca39a028dfef751233
nguyen_hoan <hoan@iastate.edu>
2017-04-30 23:24:52 -0500

    add test input

---
+++ test-resources/input/Test_alibaba2_old.java
@@ -0,0 +1,21 @@
+class Test_alibaba2_old {
+	public static String decrypt_old(PublicKey publicKey, String cipherText) throws Exception {
+		Cipher cipher = Cipher.getInstance("RSA");
+		try {
+			cipher.init(Cipher.DECRYPT_MODE, publicKey);
+		} catch (InvalidKeyException e) {
+			// for ibm jdk
+			RSAPublicKey rsaPublicKey = (RSAPublicKey) publicKey;
+			RSAPrivateKeySpec spec = new RSAPrivateKeySpec(rsaPublicKey.getModulus(), rsaPublicKey.getPublicExponent());
+			Key fakePublicKey = KeyFactory.getInstance("RSA").generatePrivate(spec);
+			cipher.init(Cipher.DECRYPT_MODE, fakePublicKey);
+		}
+
+		if (cipherText == null || cipherText.length() == 0) {
+			return cipherText;
+		}
+		byte[] cipherBytes = Base64.base64ToByteArray(cipherText);
+		byte[] plainBytes = cipher.doFinal(cipherBytes);
+		return new String(plainBytes);
+	}
+}
