c15d0c475bae8208f36564d0cc7d5d24fcc19489
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-05-02 12:07:01 +0200

    Add tests for encoding casts.

---
+++ test/egroum/EncodeCastTest.java
@@ -0,0 +1,49 @@
+package egroum;
+
+import de.tu_darmstadt.stg.mudetect.model.AUG;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static de.tu_darmstadt.stg.mudetect.model.AUGTestUtils.*;
+import static egroum.AUGBuilderTestUtils.buildAUG;
+import static egroum.EGroumDataEdge.Type.PARAMETER;
+import static egroum.EGroumDataEdge.Type.RECEIVER;
+import static org.junit.Assert.assertThat;
+
+public class EncodeCastTest {
+    @Test
+    public void encodesCast() throws Exception {
+        AUG aug = buildAUG("class C {\n" +
+                "  void m(Object obj) {\n" +
+                "    java.util.List l = (java.util.List) obj;\n" +
+                "    l.size();\n" +
+                "  }\n" +
+                "}");
+
+        assertThat(aug, hasNode(actionNodeWithLabel("List.<cast>")));
+    }
+
+    @Test @Ignore("this edge is not currently added")
+    public void addsTransitiveParameterEdgeThroughCast() throws Exception {
+        AUG aug = buildAUG("class C {\n" +
+                "  void m(java.util.List l) {\n" +
+                "    A a = (A) l.get(0);\n" +
+                "    l.remove(a);\n" +
+                "  }\n" +
+                "}");
+
+        assertThat(aug, hasEdge(actionNodeWithLabel("List.get()"), PARAMETER, actionNodeWithLabel("List.remove()")));
+    }
+
+    @Test @Ignore("this edge is not currently added")
+    public void addsTransitiveReceiverEdgeThroughCast() throws Exception {
+        AUG aug = buildAUG("class C {\n" +
+                "  void m(java.util.List l) {\n" +
+                "    A a = (A) l.get(0);\n" +
+                "    a.m();\n" +
+                "  }\n" +
+                "}");
+
+        assertThat(aug, hasEdge(actionNodeWithLabel("List.get()"), RECEIVER, actionNodeWithLabel("A.m()")));
+    }
+}
