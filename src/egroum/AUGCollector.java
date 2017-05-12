a0a7214713606ed4deda42aab59e2d9e6639504a
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-05-12 15:12:06 +0200

    Implement basic cross-project runner.

---
+++ src/egroum/AUGCollector.java
@@ -0,0 +1,26 @@
+package egroum;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class AUGCollector {
+    private final AUGConfiguration config;
+    private final Collection<EGroumGraph> augs = new ArrayList<>();
+
+    public AUGCollector(AUGConfiguration config) {
+        this.config = config;
+    }
+
+    public void collectFrom(Path path, String[] dependencies) {
+        augs.addAll(new EGroumBuilder(config).buildBatch(convertPath(path), dependencies));
+    }
+
+    private String convertPath(Path path) {
+        return path.toAbsolutePath().toString();
+    }
+
+    public Collection<EGroumGraph> getAUGs() {
+        return augs;
+    }
+}
