0018381198dcdb4a78da29192c0f7154322cc118
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-07-02 22:06:15 +0200

    Set min support to 5.

--- src/de/tu_darmstadt/stg/mubench/CrossProjectStrategy.java
+++ src/de/tu_darmstadt/stg/mubench/CrossProjectStrategy.java
@@ -236,7 +236,7 @@ class CrossProjectStrategy implements DetectionStrategy {
     private AUGMiner createMiner() {
         return new DefaultAUGMiner(new DefaultMiningConfiguration() {{
             occurenceLevel = Level.CROSS_PROJECT;
-            minPatternSupport = 8;
+            minPatternSupport = 5;
         }});
     }
 
