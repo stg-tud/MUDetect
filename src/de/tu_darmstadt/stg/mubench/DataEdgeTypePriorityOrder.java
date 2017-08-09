b93c2787f89522809ef51223f8e70261e552c69e
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-08-09 16:45:39 +0200

    Fix order priority.

--- src/de/tu_darmstadt/stg/mubench/DataEdgeTypePriorityOrder.java
+++ src/de/tu_darmstadt/stg/mubench/DataEdgeTypePriorityOrder.java
@@ -1,35 +1,40 @@
 package de.tu_darmstadt.stg.mubench;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import egroum.EGroumDataEdge;
 import egroum.EGroumEdge;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.BiPredicate;
 
 import static egroum.EGroumDataEdge.Type.*;
 
 public class DataEdgeTypePriorityOrder implements BiPredicate<EGroumEdge, EGroumEdge> {
-    private static final Map<EGroumDataEdge.Type, EGroumDataEdge.Type> EDGE_TYPE_RELATION =
-            new HashMap<EGroumDataEdge.Type, EGroumDataEdge.Type>() {{
-                put(RECEIVER, CONDITION);
-                put(RECEIVER, PARAMETER);
-                put(DEFINITION, CONDITION);
-                put(DEFINITION, PARAMETER);
-                put(CONDITION, PARAMETER);
-                // THROW
-                // FINALLY
-                // QUALIFIER
-                // ORDER
-                // CONTAINS
-            }};
+    private static final Multimap<EGroumDataEdge.Type, EGroumDataEdge.Type> EDGE_TYPE_RELATION =
+            HashMultimap.create();
+
+    static {
+        EDGE_TYPE_RELATION.put(RECEIVER, CONDITION);
+        EDGE_TYPE_RELATION.put(RECEIVER, PARAMETER);
+        EDGE_TYPE_RELATION.put(DEFINITION, CONDITION);
+        EDGE_TYPE_RELATION.put(DEFINITION, PARAMETER);
+        EDGE_TYPE_RELATION.put(CONDITION, PARAMETER);
+        // THROW
+        // FINALLY
+        // QUALIFIER
+        // ORDER
+        // CONTAINS
+    }
 
     @Override
     public boolean test(EGroumEdge edge1, EGroumEdge edge2) {
         if (edge1 instanceof EGroumDataEdge && edge2 instanceof EGroumDataEdge) {
             EGroumDataEdge.Type edge1Type = ((EGroumDataEdge) edge1).getType();
             EGroumDataEdge.Type edge2Type = ((EGroumDataEdge) edge2).getType();
-            return EDGE_TYPE_RELATION.containsKey(edge1Type) && EDGE_TYPE_RELATION.get(edge1Type) == edge2Type;
+            return EDGE_TYPE_RELATION.containsKey(edge1Type) && EDGE_TYPE_RELATION.containsEntry(edge1Type, edge2Type);
         }
         return false;
     }
