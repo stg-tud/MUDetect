331bac827d5d653370f359aaf6479800794bccf1
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-02-09 10:01:55 +0100

    Remove provided-patterns strategy from XP runner.

--- mubench/src/main/java/de/tu_darmstadt/stg/mubench/MuDetectCrossProjectRunner.java
+++ mubench/src/main/java/de/tu_darmstadt/stg/mubench/MuDetectCrossProjectRunner.java
@@ -5,7 +5,6 @@ import de.tu_darmstadt.stg.mubench.cli.MuBenchRunner;
 public class MuDetectCrossProjectRunner {
     public static void main(String[] args) throws Exception {
         new MuBenchRunner()
-                .withDetectOnlyStrategy(new ProvidedPatternsStrategy())
                 .withMineAndDetectStrategy(new CrossProjectStrategy(CrossProjectStrategy.Mode.OFFLINE))
                 .run(args);
     }
