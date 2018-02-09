8aa9dff57d544b4421f5626d46fc13573128b2f2
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-02-09 09:25:45 +0100

    Add confidence weight function.

---
+++ mubench/src/main/java/de/tu_darmstadt/stg/mudetect/ranking/ConfidenceWeightFunction.java
@@ -0,0 +1,24 @@
+package de.tu_darmstadt.stg.mudetect.ranking;
+
+import de.tu_darmstadt.stg.mudetect.mining.Model;
+import de.tu_darmstadt.stg.mudetect.model.Overlap;
+import de.tu_darmstadt.stg.mudetect.model.Overlaps;
+
+/**
+ * As proposed for DMMC by Monperrus et al. (2013).
+ */
+public class ConfidenceWeightFunction implements ViolationWeightFunction {
+    @Override
+    public double getWeight(Overlap violation, Overlaps overlaps, Model model) {
+        double patternSupport = violation.getPattern().getSupport();
+        double patternViolations = overlaps.getViolationsOfSamePattern(violation).size();
+        return patternSupport / (patternSupport + patternViolations);
+    }
+
+    @Override
+    public String getFormula(Overlap violation, Overlaps overlaps, Model model) {
+        int patternSupport = violation.getPattern().getSupport();
+        int patternViolations = overlaps.getViolationsOfSamePattern(violation).size();
+        return String.format("%d / (%d + %d)", patternSupport, patternSupport, patternViolations);
+    }
+}
