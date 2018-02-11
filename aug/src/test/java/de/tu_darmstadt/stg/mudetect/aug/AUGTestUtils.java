a35d611fba1cabbaa41935c248adf8fa85970575
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-02-11 18:59:50 +0100

    Add test util to print multiple AUGs.

--- aug/src/test/java/de/tu_darmstadt/stg/mudetect/aug/AUGTestUtils.java
+++ aug/src/test/java/de/tu_darmstadt/stg/mudetect/aug/AUGTestUtils.java
@@ -1,13 +1,27 @@
 package de.tu_darmstadt.stg.mudetect.aug;
 
-import de.tu_darmstadt.stg.mudetect.aug.model.APIUsageExample;
+import de.tu_darmstadt.stg.mudetect.aug.model.APIUsageGraph;
 import de.tu_darmstadt.stg.mudetect.aug.model.dot.DisplayAUGDotExporter;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class AUGTestUtils {
-    public static void exportAUGasPNG(APIUsageExample aug, String filepath) throws IOException, InterruptedException {
-        new DisplayAUGDotExporter().toPNGFile(aug, new File(filepath));
+    public static <T extends APIUsageGraph> void exportAUGsAsPNG(Collection<T> augs, String filePath, String baseName) {
+        Iterator<? extends APIUsageGraph> it = augs.iterator();
+        for (int i = 0; it.hasNext() ; i++) {
+            APIUsageGraph aug = it.next();
+            exportAUGasPNG(aug, new File(filePath, baseName + "-" + i + ".png").getPath());
+        }
+    }
+
+    public static void exportAUGasPNG(APIUsageGraph aug, String filePath) {
+        try {
+            new DisplayAUGDotExporter().toPNGFile(aug, new File(filePath));
+        } catch (Exception e) {
+            throw new RuntimeException("failed to export DOT as PNG", e);
+        }
     }
 }
