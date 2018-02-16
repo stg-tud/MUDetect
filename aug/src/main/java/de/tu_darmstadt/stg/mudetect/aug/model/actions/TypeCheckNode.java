50ebd7ccceb2193a5ee0d9f9481be8efcb6b2070
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-02-16 16:23:04 +0100

    Make type check an operator.

--- aug/src/main/java/de/tu_darmstadt/stg/mudetect/aug/model/actions/TypeCheckNode.java
+++ aug/src/main/java/de/tu_darmstadt/stg/mudetect/aug/model/actions/TypeCheckNode.java
@@ -1,12 +1,21 @@
 package de.tu_darmstadt.stg.mudetect.aug.model.actions;
 
-public class TypeCheckNode extends MethodCallNode {
+public class TypeCheckNode extends OperatorNode {
+    private final String targetTypeName;
+
     public TypeCheckNode(String targetTypeName) {
-        super(targetTypeName, "<instanceof>");
+        super("<instanceof>");
+        this.targetTypeName = targetTypeName;
     }
 
     public TypeCheckNode(String targetTypeName, int sourceLineNumber) {
-        super(targetTypeName, "<instanceof>", sourceLineNumber);
+        super("<instanceof>", sourceLineNumber);
+        this.targetTypeName = targetTypeName;
+    }
+
+    @Override
+    public String getLabel() {
+        return targetTypeName + ".<instanceof>";
     }
 
     @Override
