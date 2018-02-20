042ce7a7891df0f7b7a956c3dba0296ac2ffd28a
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-02-20 14:18:13 +0100

    Extract duplication.

--- mubench/src/test/java/de/tu_darmstadt/stg/mudetect/ranking/AverageWeightFunctionTest.java
+++ mubench/src/test/java/de/tu_darmstadt/stg/mudetect/ranking/AverageWeightFunctionTest.java
@@ -6,17 +6,18 @@ import de.tu_darmstadt.stg.mudetect.model.Overlaps;
 import org.junit.Test;
 
 import static de.tu_darmstadt.stg.mudetect.model.TestOverlapBuilder.someOverlap;
+import static de.tu_darmstadt.stg.mudetect.ranking.RankingTestUtils.w;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static de.tu_darmstadt.stg.mudetect.utils.SetUtils.asSet;
 
 public class AverageWeightFunctionTest {
     @Test
-    public void takesOverSingleWeight() throws Exception {
+    public void takesOverSingleWeight() {
         Overlap instance = someOverlap();
         Overlaps overlaps = new Overlaps();
         Model model = () -> asSet(instance.getPattern());
-        ViolationWeightFunction weightFunction = new AverageWeightFunction(w((v, os, m) -> 42f));
+        ViolationWeightFunction weightFunction = new AverageWeightFunction(w((v, os, m) -> 42.0));
 
         double weight = weightFunction.getWeight(instance, overlaps, model);
 
@@ -24,35 +25,17 @@ public class AverageWeightFunctionTest {
     }
 
     @Test
-    public void addsAndNormalizesTwoWeights() throws Exception {
+    public void addsAndNormalizesTwoWeights() {
         Overlap instance = someOverlap();
         Overlaps overlaps = new Overlaps();
         Model model = () -> asSet(instance.getPattern());
         ViolationWeightFunction weightFunction = new AverageWeightFunction(
-                w((v, os, m) -> 2f),
-                w((v, os, m) -> 4f));
+                w((v, os, m) -> 2.0),
+                w((v, os, m) -> 4.0));
 
         double weight = weightFunction.getWeight(instance, overlaps, model);
 
         assertThat(weight, is(3.0));
     }
 
-    private static ViolationWeightFunction w(Function<Overlap, Overlaps, Model, Float> weight) {
-        return new ViolationWeightFunction() {
-            @Override
-            public double getWeight(Overlap violation, Overlaps overlaps, Model model) {
-                return weight.apply(violation, overlaps, model);
-            }
-
-            @Override
-            public String getFormula(Overlap violation, Overlaps overlaps, Model model) {
-                throw new UnsupportedOperationException();
-            }
-        };
-    }
-
-    @FunctionalInterface
-    interface Function<A, B, C, R> {
-        R apply(A a, B b, C c);
-    }
 }
---
+++ mubench/src/test/java/de/tu_darmstadt/stg/mudetect/ranking/RankingTestUtils.java
@@ -0,0 +1,26 @@
+package de.tu_darmstadt.stg.mudetect.ranking;
+
+import de.tu_darmstadt.stg.mudetect.model.Overlap;
+import de.tu_darmstadt.stg.mudetect.model.Overlaps;
+import edu.iastate.cs.mudetect.mining.Model;
+
+public class RankingTestUtils {
+    static ViolationWeightFunction w(Function<Overlap, Overlaps, Model, Double> weight) {
+        return new ViolationWeightFunction() {
+            @Override
+            public double getWeight(Overlap violation, Overlaps overlaps, Model model) {
+                return weight.apply(violation, overlaps, model);
+            }
+
+            @Override
+            public String getFormula(Overlap violation, Overlaps overlaps, Model model) {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    @FunctionalInterface
+    interface Function<A, B, C, R> {
+        R apply(A a, B b, C c);
+    }
+}
