8b8fe359080125a9f97a4748ee1b70708b66b305
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-02-20 14:53:48 +0100

    Provide a ranking-comparison strategy and runner.

---
+++ mubench/src/main/java/de/tu_darmstadt/stg/mubench/AlternativeRankingAndFilterStrategy.java
@@ -0,0 +1,107 @@
+package de.tu_darmstadt.stg.mubench;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import de.tu_darmstadt.stg.mudetect.AlternativePatternInstancePredicate;
+import de.tu_darmstadt.stg.mudetect.model.Overlap;
+import de.tu_darmstadt.stg.mudetect.model.Overlaps;
+import de.tu_darmstadt.stg.mudetect.model.Violation;
+import de.tu_darmstadt.stg.mudetect.ranking.*;
+import edu.iastate.cs.mudetect.mining.Model;
+
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+import static de.tu_darmstadt.stg.mudetect.AlternativeViolationPredicate.firstAlternativeViolation;
+
+public class AlternativeRankingAndFilterStrategy implements BiFunction<Overlaps, Model, List<Violation>> {
+    private final Collection<ViolationWeightFunction> strategies;
+    private final AlternativePatternInstancePredicate alternativePatternInstancePredicate;
+
+    public AlternativeRankingAndFilterStrategy() {
+        strategies = new ArrayList<>();
+        strategies.add(new RarenessWeightFunction());
+        strategies.add(new DefectIndicatorWeightFunction());
+        strategies.add(new ConfidenceWeightFunction());
+
+        Collection<Set<ViolationWeightFunction>> permutations = Sets.powerSet(ImmutableSet.of(
+                new OverlapWithoutEdgesToMissingNodesWeightFunction(new ConstantNodeWeightFunction()),
+                new PatternSupportWeightFunction(),
+                new PatternViolationsWeightFunction(),
+                new ViolationSupportWeightFunction(),
+                new PatternUniquenessWeightFunction()
+        ));
+        for (Set<ViolationWeightFunction> permutation : permutations) {
+            strategies.add(new ProductWeightFunction(permutation.toArray(new ViolationWeightFunction[0])));
+        }
+        alternativePatternInstancePredicate = new AlternativePatternInstancePredicate();
+    }
+
+    @Override
+    public List<Violation> apply(Overlaps overlaps, Model model) {
+        SortedMap<String, List<Violation>> alternativeRankings = buildAlternativeRankings(overlaps, model);
+
+        List<Violation> violations = new ArrayList<>();
+        for (Overlap violationOverlap : overlaps.getViolations()) {
+            violations.add(new Violation(violationOverlap, 1.0, getRankInfo(alternativeRankings, violationOverlap)));
+        }
+        return violations;
+    }
+
+    private SortedMap<String, List<Violation>> buildAlternativeRankings(Overlaps overlaps, Model model) {
+        SortedMap<String, List<Violation>> alternativelyRankedViolations = new TreeMap<>();
+        for (ViolationWeightFunction strategy : strategies) {
+            List<Violation> rankedViolations = new WeightRankingStrategy(strategy).rankViolations(overlaps, model).stream()
+                    .filter(violation -> isNotAlternativePatternInstance(violation, overlaps))
+                    .filter(firstAlternativeViolation())
+                    .collect(Collectors.toList());
+
+            alternativelyRankedViolations.put(strategy.getId(), rankedViolations);
+        }
+        return alternativelyRankedViolations;
+    }
+
+    private boolean isNotAlternativePatternInstance(Violation violation, Overlaps overlaps) {
+        Overlap overlap = violation.getOverlap();
+        Set<Overlap> instancesInViolationTarget = overlaps.getInstancesInSameTarget(overlap);
+        return !alternativePatternInstancePredicate.test(overlap, instancesInViolationTarget);
+    }
+
+    private String getRankInfo(SortedMap<String, List<Violation>> alternativeRankings, Overlap violationOverlap) {
+        StringBuilder rankInfo =
+                new StringBuilder("<table style=\"display:block;height:400px;width:500px;overflow:scroll;border: 1px solid black;\">")
+                .append("<thead><tr><th>Strategy</th><th>Rank</th><th>Desc</th></tr></thead><tbody>");
+        for (String strategyId : alternativeRankings.keySet()) {
+            List<Violation> alternativeRanking = alternativeRankings.get(strategyId);
+            appendRankInfo(rankInfo, strategyId, alternativeRanking, violationOverlap);
+        }
+        rankInfo.append("</tbody></table>");
+        return rankInfo.toString();
+    }
+
+    private void appendRankInfo(StringBuilder rankInfo, String strategyId, List<Violation> alternativeRanking, Overlap violationOverlap) {
+        int rank = 0;
+        Violation violation = null;
+        for (; rank < alternativeRanking.size(); rank++) {
+            if (alternativeRanking.get(rank).getOverlap() == violationOverlap) {
+                violation = alternativeRanking.get(rank);
+                break;
+            }
+        }
+
+        appendRankInfo(rankInfo, strategyId, rank, violation);
+    }
+
+    private void appendRankInfo(StringBuilder rankInfo, String strategyId, int rank, Violation violation) {
+        rankInfo.append("<tr><td>").append(strategyId);
+        if (violation != null) {
+            rankInfo.append("</td><td>").append(rank)
+                    .append("</td><td>").append(violation.getConfidenceString());
+        } else {
+            rankInfo.append("</td><td>").append("-")
+                    .append("</td><td>").append("filtered");
+        }
+        rankInfo.append("</td></tr>");
+    }
+}
---
+++ mubench/src/main/java/de/tu_darmstadt/stg/mubench/AlternativeRankingIntraProjectStrategy.java
@@ -0,0 +1,20 @@
+package de.tu_darmstadt.stg.mubench;
+
+import de.tu_darmstadt.stg.mudetect.*;
+import de.tu_darmstadt.stg.mudetect.overlapsfinder.AlternativeMappingsOverlapsFinder;
+import edu.iastate.cs.mudetect.mining.MinPatternActionsModel;
+import edu.iastate.cs.mudetect.mining.Model;
+
+public class AlternativeRankingIntraProjectStrategy extends IntraProjectStrategy {
+    @Override
+    protected MuDetect createDetector(Model model) {
+        return new MuDetect(
+                new MinPatternActionsModel(model, 2),
+                new AlternativeMappingsOverlapsFinder(new DefaultOverlapFinderConfig(new DefaultMiningConfiguration())),
+                new FirstDecisionViolationPredicate(
+                        new MissingDefPrefixNoViolationPredicate(),
+                        new MissingCatchNoViolationPredicate(),
+                        new MissingElementViolationPredicate()),
+                new AlternativeRankingAndFilterStrategy());
+    }
+}
