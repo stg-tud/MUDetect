2a1047d2baf5cc7b6a73055fd2191c7dbbb34ed2
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-02-23 11:54:20 +0100

    Treat sel and rep equally in detection.

--- mubench/src/main/java/de/tu_darmstadt/stg/mubench/DefaultOverlapFinderConfig.java
+++ mubench/src/main/java/de/tu_darmstadt/stg/mubench/DefaultOverlapFinderConfig.java
@@ -5,10 +5,21 @@ import de.tu_darmstadt.stg.mudetect.matcher.EquallyLabelledNodeMatcher;
 import de.tu_darmstadt.stg.mudetect.overlapsfinder.AlternativeMappingsOverlapsFinder;
 import edu.iastate.cs.mudetect.mining.Configuration;
 
+import java.util.HashSet;
+
+import static de.tu_darmstadt.stg.mudetect.aug.model.controlflow.ConditionEdge.ConditionType.REPETITION;
+import static de.tu_darmstadt.stg.mudetect.aug.model.controlflow.ConditionEdge.ConditionType.SELECTION;
+
 class DefaultOverlapFinderConfig extends AlternativeMappingsOverlapsFinder.Config {
     DefaultOverlapFinderConfig(Configuration config) {
+        // SMELL this is a hack, we should introduce a edgeLabel function instead, to handle this equally in mining and detection
+        HashSet<Object> equivalentEdgeLabels = new HashSet<>();
+        equivalentEdgeLabels.add(SELECTION.getLabel());
+        equivalentEdgeLabels.add(REPETITION.getLabel());
+
         isStartNode = super.isStartNode.and(new VeryPrevalentNodePredicate().negate());
         nodeMatcher = new EquallyLabelledNodeMatcher(config.nodeToLabel);
         edgeOrder = new DataEdgeTypePriorityOrder();
+        edgeMatcher = super.edgeMatcher.or((el1, el2) -> equivalentEdgeLabels.contains(el1) && equivalentEdgeLabels.contains(el2));
     }
 }
