b6a78c9753a06acff1e5d2d7827eca5ff6077ad8
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-09-13 15:40:42 +0200

    Update paper examples.

--- src2egroum2aug/src/test/java/edu/iastate/cs/egroum/aug/PaperExamples.java
+++ src2egroum2aug/src/test/java/edu/iastate/cs/egroum/aug/PaperExamples.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,16 +17,44 @@ public class PaperExamples {
     @Test
     public void paperExample() throws Exception {
         printAUG("AUG",
-                "void m(String file) {" +
-                "  if (file != null) {" +
-                "    try {" +
-                "      java.io.FileInputStream fis = new java.io.FileInputStream(file);" +
-                "      return fis.read();" +
-                "    } catch(java.io.FileNotFoundException e) {" +
-                "      handle(e);" +
-                "    }" +
-                "  }" +
-                "}");
+                "void m(String file) {\n" +
+                "  if (file != null) {\n" +
+                "    try {\n" +
+                "      return new java.io.FileInputStream(file).read();\n" +
+                "    } catch(java.io.FileNotFoundException e) {\n" +
+                "      Foo.handle(e);\n" +
+                "    }\n" +
+                "  }\n" +
+                "}\n");
+
+        printAUG("AUG2",
+                "void m(String file) {\n" +
+                        "  if (file != null) {\n" +
+                        "    try {\n" +
+                        "      java.io.FileInputStream fis = new java.io.FileInputStream(file);\n" +
+                        "      int i = 0;    \n" +
+                        "      while ((i = fis.read()) != -1) {\n" +
+                        "          Foo.doSomething(i);    \n" +
+                        "      }\n" +
+                        "    } catch(java.io.FileNotFoundException e) {" +
+                        "      Foo.handle(e);" +
+                        "    }" +
+                        "  }" +
+                        "}");
+
+        printAUG("IteratorMisuse",
+                "void m(Collection<String> files) {\n" +
+                        "Iterator<String> it = files.iterator();\n" +
+                        "String first = it.next();\n" +
+                        "}");
+
+        printAUG("IteratorPattern",
+                "void m(Collection<String> files) {\n" +
+                        "Iterator<String> it = files.iterator();\n" +
+                        "if (it.hasNext()) {\n" +
+                        "String first = it.next();\n" +
+                        "}\n" +
+                        "}");
     }
 
     @Test
@@ -45,18 +75,37 @@ public class PaperExamples {
     @Test
     public void anonymousClassInstances() throws Exception {
         printAUG("anonymous-class-instance",
-                "void m() {" +
-                "  new Thread(new Runnable() {\n" +
+                "public static void main(String[] args) {" +
+                "  SwingUtilities.invokeLater(new Runnable() {\n" +
                 "    @Override\n" +
                 "    public void run() {\n" +
-                "      new Object();\n" +
+                "      JFrame f = new JFrame(\"Main Window\");\n" +
+                "      // add components...\n" +
+                "      f.setVisible(true);\n" +
                 "    }\n" +
-                "  }).start();" +
+                "  });" +
                 "}");
     }
 
     private void printAUG(String exampleName, String code) throws IOException, InterruptedException {
-        APIUsageExample aug = AUGBuilderTestUtils.buildAUGForMethod(code);
+        APIUsageExample aug = AUGBuilderTestUtils.buildAUGForMethod(code, new AUGConfiguration() {
+            {
+                minStatements = 0;
+                groum = false;
+
+                collapseIsomorphicSubgraphs = true;
+
+                collapseTemporaryDataNodes = false;
+                collapseTemporaryDataNodesIncomingToControlNodes = true;
+
+                encodeUnaryOperators = false;
+                encodeConditionalOperators = false;
+
+                buildTransitiveDataEdges = false;
+
+                removeImplementationCode = 2;
+            }
+        });
         List<Node> scaffoldNodes = aug.vertexSet().stream()
                 .filter(node -> node instanceof DataNode && ((DataNode) node).getType().equals("C")).collect(Collectors.toList());
         for (Node scaffoldNode : scaffoldNodes) {
